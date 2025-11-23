d23e771be0a02995ee73514ea46eb04c9192c4fd
binninwl <197739058+moonzybinninwl@users.noreply.github.com>
2025-11-23 14:31:22 +1100

    Create Filter.hpp

---
+++ Filter.hpp
@@ -0,0 +1,39 @@
+inline static const std::unordered_set<std::string> RestrictedFunctionCalls =
+{
+        "openvideosfolder","openscreenshotsfolder","getrobuxbalance","performpurchase",
+        "promptbundlepurchase","promptnativepurchase","promptproductpurchase","promptpurchase",
+        "promptthirdpartypurchase","publish","getmessageid","openbrowserwindow",
+        "requestinternal","executejavascript","togglerecording","takescreenshot",
+        "httprequestasync","getlast","sendcommand","getasync","getasyncfullurl",
+        "requestasync","makerequest","postasync","httppost","postasyncfullurl",
+        "performpurchasev2","promptgamepasspurchase","promptrobloxpurchase",
+        "opennativeoverlay","addcorescriptlocal","emithybridevent","returntojavascript",
+        "call","openurl","savescriptprofilingdata","getprotocolmethodrequestmessageid",
+        "getprotocolmethodresponsemessageid","publishprotocolmethodrequest",
+        "publishprotocolmethodresponse","subscribe","subscribetoprotocolmethodrequest",
+        "subscribetoprotocolmethodresponse","promptnativepurchasewithlocalplayer",
+        "promptcollectiblespurchase","performbulkpurchase","performcancelsubscription",
+        "performsubscriptionpurchase","performsubscriptionpurchasev2",
+        "preparecollectiblespurchase","promptbulkpurchase","promptcancelsubscription",
+        "promptpremiumpurchase","promptsubscriptionpurchase","openwechatauthwindow",
+        "requestlimitedasync","run","capturescreenshot","createpostasync",
+        "deletecapture","deletecapturesasync","getcapturefilepathasync",
+        "savecapturetoexternalstorage","savecapturestoexternalstorageasync",
+        "getcaptureuploaddataasync","retrievecaptures","savescreenshotcapture",
+        "getcredentialsheaders","getdeviceintegritytoken","getdeviceintegritytokenyield",
+        "nopromptcreateoutfit","nopromptdeleteoutfit","nopromptrenameoutfit",
+        "nopromptsaveavatar","nopromptsaveavatarthumbnailcustomization","nopromptsetfavorite",
+        "nopromptupdateoutfit","performcreateoutfitwithdescription","performrenameoutfit",
+        "performsaveavatarwithdescription","performsetfavorite","performupdateoutfit",
+        "promptcreateoutfit","promptdeleteoutfit","promptrenameoutfit","promptsaveavatar",
+        "promptsetfavorite","promptupdateoutfit","promptimportfile","promptimportfiles",
+        "getusersubscriptionpaymenthistoryasync","getusersubscriptiondetailsinternalasync",
+        "callfunction","bindcoreactive","reportabuse","reportabusev3","reportchatabuse",
+        "load", "getusersubscriptionstatusasync"
+};
+
+inline static const std::unordered_set<std::string> RstrictedServiceCalls =
+{
+    "accountservice","testservice","omnirecommendationsservice","messagebusservice",
+    "linkingservice","browserservice","opencloudservice","scriptprofilerservice"
+};
